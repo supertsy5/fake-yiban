b6bb43de22b580b0cdc33900fee2d71659b27dd5
supertsy5 <22672297+supertsy5@users.noreply.github.com>
2021-10-10 11:58:53 +0800

    20211010

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,13 @@
 module.exports = {
     publicPath: process.env.NODE_ENV === 'production'
         ? '/fake-yiban/dist/'
-        : '/'
+        : '/',
+    pwa: {
+        name: 'Fake Yiban',
+        themeColor: '#6cb0f9',
+        manifestOptions: {
+            name: 'Fake Yiban: 不一般的易班',
+            backgroundColor: '#6cb0f9'
+        }
+    }
 }
