895731cc2050b7ab0ff5a943c4f139f1e1da56a7
supertsy5 <22672297+supertsy5@users.noreply.github.com>
2021-10-10 12:00:12 +0800

    20211010

---
+++ dist/precache-manifest.4ba5000d8d0d8daad9d6ec52290beca4.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46c63809bb1057d82bc9",
+    "url": "/fake-yiban/dist/css/app.e02ebeb2.css"
+  },
+  {
+    "revision": "74a47c1b6c7d01840f5caa86d7260f16",
+    "url": "/fake-yiban/dist/favicon.png"
+  },
+  {
+    "revision": "0009ddfac2b38685411c640d5b23e101",
+    "url": "/fake-yiban/dist/favicon192.png"
+  },
+  {
+    "revision": "5e8ced678248d0f8a44c2e0752bc3b02",
+    "url": "/fake-yiban/dist/img/approved.5e8ced67.png"
+  },
+  {
+    "revision": "bade37cb279c36d23f3521b94c2fd4b8",
+    "url": "/fake-yiban/dist/img/back.bade37cb.svg"
+  },
+  {
+    "revision": "b2ffde2613599160449d0362bb69ccac",
+    "url": "/fake-yiban/dist/img/close.b2ffde26.svg"
+  },
+  {
+    "revision": "c6c92fec7fe8ef49f66a014c25049ab2",
+    "url": "/fake-yiban/dist/img/menu.c6c92fec.svg"
+  },
+  {
+    "revision": "5272d6497366ff086c68c82680fd1c90",
+    "url": "/fake-yiban/dist/index.html"
+  },
+  {
+    "revision": "46c63809bb1057d82bc9",
+    "url": "/fake-yiban/dist/js/app.7dfe2342.js"
+  },
+  {
+    "revision": "11812159e283fdda5159",
+    "url": "/fake-yiban/dist/js/chunk-vendors.0ca36a88.js"
+  },
+  {
+    "revision": "810a7c532dea72c8366a133a117cce43",
+    "url": "/fake-yiban/dist/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/fake-yiban/dist/robots.txt"
+  }
+]);
