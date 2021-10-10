d5a554f9020a6b9c4b74bb0375daa4fc69f62ebe
supertsy5 <22672297+supertsy5@users.noreply.github.com>
2021-10-10 12:22:36 +0800

    20211010

--- vue.config.js
+++ vue.config.js
@@ -8,7 +8,7 @@ module.exports = {
         msTileColor: '#6cb0f9',
         manifestOptions: {
             name: 'Fake Yiban: 不一般的易班',
-            backgroundColor: '#6cb0f9'
+            background_olor: '#6cb0f9'
         }
     }
 }
