efcf94f6204d56a7c1beffaf5c5d4a4f05fca5ae
supertsy5 <22672297+supertsy5@users.noreply.github.com>
2021-10-12 19:46:26 +0800

    20211012

---
+++ dist/precache-manifest.a0e968a3e8852210c62a8b017b2e714d.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2963940f3f1f670f5615",
+    "url": "/fake-yiban/dist/css/app.9072a9de.css"
+  },
+  {
+    "revision": "2963940f3f1f670f5615",
+    "url": "/fake-yiban/dist/css/app.9072a9de.css.map"
+  },
+  {
+    "revision": "1ba2ae710d927f13d483fd5d1e548c9b",
+    "url": "/fake-yiban/dist/favicon.ico"
+  },
+  {
+    "revision": "5e8ced678248d0f8a44c2e0752bc3b02",
+    "url": "/fake-yiban/dist/img/approved.5e8ced67.png"
+  },
+  {
+    "revision": "bade37cb279c36d23f3521b94c2fd4b8",
+    "url": "/fake-yiban/dist/img/back.bade37cb.svg"
+  },
+  {
+    "revision": "b2ffde2613599160449d0362bb69ccac",
+    "url": "/fake-yiban/dist/img/close.b2ffde26.svg"
+  },
+  {
+    "revision": "f130a0b70e386170cf6f011c0ca8c4f4",
+    "url": "/fake-yiban/dist/img/icons/android-chrome-192x192.png"
+  },
+  {
+    "revision": "0ff1bc4d14e5c9abcacba7c600d97814",
+    "url": "/fake-yiban/dist/img/icons/android-chrome-512x512.png"
+  },
+  {
+    "revision": "845a39478d0e2d4d5d32a092de2de250",
+    "url": "/fake-yiban/dist/img/icons/android-chrome-maskable-192x192.png"
+  },
+  {
+    "revision": "2695f5feb66cdb0c6f09d0e415824cf9",
+    "url": "/fake-yiban/dist/img/icons/android-chrome-maskable-512x512.png"
+  },
+  {
+    "revision": "936d6e411cabd71f0e627011c3f18fe2",
+    "url": "/fake-yiban/dist/img/icons/apple-touch-icon-120x120.png"
+  },
+  {
+    "revision": "1a034e64d80905128113e5272a5ab95e",
+    "url": "/fake-yiban/dist/img/icons/apple-touch-icon-152x152.png"
+  },
+  {
+    "revision": "c43cd371a49ee4ca17ab3a60e72bdd51",
+    "url": "/fake-yiban/dist/img/icons/apple-touch-icon-180x180.png"
+  },
+  {
+    "revision": "9a2b5c0f19de617685b7b5b42464e7db",
+    "url": "/fake-yiban/dist/img/icons/apple-touch-icon-60x60.png"
+  },
+  {
+    "revision": "af28d69d59284dd202aa55e57227b11b",
+    "url": "/fake-yiban/dist/img/icons/apple-touch-icon-76x76.png"
+  },
+  {
+    "revision": "66830ea6be8e7e94fb55df9f7b778f2e",
+    "url": "/fake-yiban/dist/img/icons/apple-touch-icon.png"
+  },
+  {
+    "revision": "4bb1a55479d61843b89a2fdafa7849b3",
+    "url": "/fake-yiban/dist/img/icons/favicon-16x16.png"
+  },
+  {
+    "revision": "98b614336d9a12cb3f7bedb001da6fca",
+    "url": "/fake-yiban/dist/img/icons/favicon-32x32.png"
+  },
+  {
+    "revision": "b89032a4a5a1879f30ba05a13947f26f",
+    "url": "/fake-yiban/dist/img/icons/msapplication-icon-144x144.png"
+  },
+  {
+    "revision": "058a3335d15a3eb84e7ae3707ba09620",
+    "url": "/fake-yiban/dist/img/icons/mstile-150x150.png"
+  },
+  {
+    "revision": "4e857233cbd3bb2d2db4f78bed62a52f",
+    "url": "/fake-yiban/dist/img/icons/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "c6c92fec7fe8ef49f66a014c25049ab2",
+    "url": "/fake-yiban/dist/img/menu.c6c92fec.svg"
+  },
+  {
+    "revision": "5670cd9a4108a12a8a2a5402c28beeb4",
+    "url": "/fake-yiban/dist/index.html"
+  },
+  {
+    "revision": "2963940f3f1f670f5615",
+    "url": "/fake-yiban/dist/js/app.b3852b04.js"
+  },
+  {
+    "revision": "2963940f3f1f670f5615",
+    "url": "/fake-yiban/dist/js/app.b3852b04.js.map"
+  },
+  {
+    "revision": "9607762db2b1a730c94e",
+    "url": "/fake-yiban/dist/js/chunk-vendors.a8f0d4bb.js"
+  },
+  {
+    "revision": "9607762db2b1a730c94e",
+    "url": "/fake-yiban/dist/js/chunk-vendors.a8f0d4bb.js.map"
+  }
+]);
